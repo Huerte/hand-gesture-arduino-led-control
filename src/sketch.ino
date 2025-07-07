85cb90ce6539800e259197f830c228d48796a296
Huerte <huertejerald@gmail.com>
2025-07-07 13:55:53 +0800

    refactor the script.ino

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,18 +1,12 @@
-const int LED_ONE = 8;
-const int LED_TWO = 9;
-const int LED_THREE = 10;
-const int LED_FOUR = 11;
-const int LED_FIVE = 12;
+const int LED_PIN[5] = {8, 9, 10, 11, 12};
 
 void setup() {
 
   Serial.begin(9600);
 
-  pinMode(LED_ONE, OUTPUT);
-  pinMode(LED_TWO, OUTPUT);
-  pinMode(LED_THREE, OUTPUT);
-  pinMode(LED_FOUR, OUTPUT);
-  pinMode(LED_FIVE, OUTPUT);
+  for (const int PIN: LED_PIN) {
+    pinMode(PIN, OUTPUT);
+  }
 
 }
 
@@ -22,18 +16,8 @@ void loop() {
 
     char msg = Serial.read();
 
-    if (msg == '1') {
-      digitalWrite(LED_ONE, HIGH);
-      digitalWrite(LED_TWO, HIGH);
-      digitalWrite(LED_THREE, HIGH);
-      digitalWrite(LED_FOUR, HIGH);
-      digitalWrite(LED_FIVE, HIGH);
-    } else if (msg == '0') {
-      digitalWrite(LED_ONE, LOW);
-      digitalWrite(LED_TWO, LOW);
-      digitalWrite(LED_THREE, LOW);
-      digitalWrite(LED_FOUR, LOW);
-      digitalWrite(LED_FIVE, LOW);
+    for (const int PIN: LED_PIN) {
+      digitalWrite(PIN, (msg == '1')? HIGH: LOW);
     }
 
   }
